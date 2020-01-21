d0aafd68cac7e671e7be6d50b33e49f061d98823
이성재 <adfffewr@naver.com>
2020-01-21 18:06:45 +0900

    header 구성 작업

--- front/src/Components/Common/Header.js
+++ front/src/Components/Common/Header.js
@@ -1,19 +1,118 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const HeaderBox = styled.header`
+  width: 100%;
+  height: 50px;
+  background-color: #6c63ff;
+  position: relative;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  h1 {
+    margin-right: 10px;
+  }
+`;
+
+const HeaderNav = styled.nav`
+  ul {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  ul li {
+    padding: 0 10px;
+  }
+  ul li a {
+    color: #fff;
+    transition: color 0.2s;
+  }
+  ul li a:hover {
+    color: rgba(255, 255, 255, 0.75);
+  }
+  @media screen and (max-width: 768px) {
+    position: absolute;
+    left: 0%;
+    top: 50px;
+    width: 100%;
+    background-color: #6c63ff;
+    ul li {
+      width: 100%;
+    }
+    ul li a {
+      display: block;
+      padding: 10px 0;
+    }
+  }
+`;
+
+const HeaderNavBtn = styled.button`
+  position: absolute;
+  right: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 27px;
+  height: 20px;
+  display: none;
+  span {
+    position: absolute;
+    left: 50%;
+    width: 100%;
+    height: 2px;
+    background-color: #fff;
+  }
+  ${props => console.log(props)}
+  .line1 {
+    top: 0;
+    transform: translate(-50%);
+  }
+  .line2 {
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .line3 {
+    top: 100%;
+    transform: translate(-50%, -100%);
+  }
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
 const Header = () => {
+  const [active, setActive] = useState('');
+
+  const NavBtnClick = () => {
+    setActive(active === '' ? 'active' : '');
+  };
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/post">Post</Link>
-          </li>
-        </ul>
-      </header>
+      <HeaderBox>
+        <HeaderContent className="max_box">
+          <h1>
+            <Link to="/">로고</Link>
+          </h1>
+          <HeaderNav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/post">Post</Link>
+              </li>
+            </ul>
+          </HeaderNav>
+          <HeaderNavBtn active={active} onClick={NavBtnClick}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </HeaderNavBtn>
+        </HeaderContent>
+      </HeaderBox>
     </>
   );
 };
--- front/src/Components/GlobalStyles.js
+++ front/src/Components/GlobalStyles.js
@@ -1,7 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 const GlobalStyles = createGlobalStyle`
-  ${reset};  
+  ${reset};
+  @font-face { font-family: 'RIDIBatang'; src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/RIDIBatang.woff') format('woff'); font-weight: normal; font-style: normal; }
+  html{font-size:16px;}
+  body{background:#fafafa;}
+  body *{font-family:RIDIBatang; color:#333;}
+  a{text-decoration:none;}
+  button{background:none; border:none; outline:none; cursor:pointer; padding:0;}
+  .max_box{max-width:1230px; margin:0 auto; padding:0 15px; box-sizing:border-box;}
+  .clearfix:after{content:''; display:block; clear:both;}
+  
+
 `;
 
 export default GlobalStyles;
