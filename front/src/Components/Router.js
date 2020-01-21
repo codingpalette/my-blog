4ddc2fdbc76b9338ed8c2d795ac71315d1b9b143
이성재 <adfffewr@naver.com>
2020-01-21 15:08:46 +0900

    코드 스플리팅 적용

--- front/src/Components/Router.js
+++ front/src/Components/Router.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import { Home, Post } from '../Routes';
 import Header from './Common/Header';
+const Home = lazy(() => import('../Routes/Home'));
+const Post = lazy(() => import('../Routes/Post'));
 
 const Router = () => (
   <BrowserRouter>
     <>
       <Header />
-      <Switch>
-        <Route path="/" component={Home} exact></Route>
-        <Route path="/post" component={Post} exact></Route>
-        <Redirect from="*" to="/"></Redirect>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" component={Home} exact></Route>
+          <Route path="/post" component={Post} exact></Route>
+          <Redirect from="*" to="/"></Redirect>
+        </Switch>
+      </Suspense>
     </>
   </BrowserRouter>
 );
